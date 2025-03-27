ed80b16c434146e125eaa431509fc6c7d3a222a5
AE Ringuelet <eze87.lp@hotmail.com>
2025-03-27 11:41:43 +0100

    commiting a lot of changes to avoid loosing track of changes from now.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	
+	experimental: {
+		svg: {
+			mode: "sprite",
+		},
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
