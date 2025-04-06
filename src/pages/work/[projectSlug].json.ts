94b757b203fd1e15059073f9a814293d3accfc48
AE Ringuelet <eze87.lp@hotmail.com>
2025-04-06 14:35:10 +0200

    add simple 404 page, add work page, modify layout to properly apply padding to children, add some assets

---
+++ src/pages/work/[projectSlug].json.ts
@@ -0,0 +1,25 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async ({ params }) => {
+    const slug = params.projectSlug;
+    const project = {
+        slug,
+        title: 'Project title',
+        description:
+            'Project description Project description Project description Project description Project description Project description Project description Project description',
+        startDate: '2022-01-01',
+        endDate: '2022-12-31',
+        image: 'https://placekitten.com/200/300'
+    };
+
+    if (!slug) {
+        return new Response(null, {
+            status: 400,
+            statusText: 'Bad Request: Missing product slug'
+        });
+    }
+
+    return new Response(JSON.stringify(project), {
+        status: 200
+    });
+};
