14ffbb8fd774f437b55be7b515d47f739d21652c
AE Ringuelet <eze87.lp@hotmail.com>
2025-04-06 18:10:34 +0200

    change to use netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,7 +16,5 @@ export default defineConfig({
         plugins: [tailwindcss()]
     },
 
-    adapter: node({
-        mode: 'standalone'
-    })
+    adapter: netlify()
 });
